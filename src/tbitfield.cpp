cdfa165bf9b8b2d432ad77c1e69ac244160866cc
Vlad Fedotov <fedotovvlad@list.ru>
2018-09-14 12:19:52 +0300

    14-09

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,53 +9,85 @@
 
 TBitField::TBitField(int len)
 {
+    BitLen = len;
+    MemLen = ((len)/((sizeof(TELEM))*8));
+    if ((len)%((sizeof(TELEM))*8) > 0)
+    {
+        MemLen++;
+    }
+    TELEM * pMem = new TELEM[MemLen];
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+    BitLen = bf.BitLen;
+    MemLen = bf.MemLen;
+    TELEM * pMem = new TELEM[MemLen];
+    for (int i = 0; i < MemLen; i++)
+    {
+        pMem[i] = bf.pMem[i];
+    }
 }
 
 TBitField::~TBitField()
 {
+    delete pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+    int SizeOfTelem = sizeof(TELEM)*8;
+    return n / SizeOfTelem;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+    int number = n % (sizeof(TELEM)*8);
+    TELEM mask = 0;
+    mask = 1 << number;
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+  return BitLen;
+  //return 0;
 }
 
 void TBitField::SetBit(const int n) // установить бит
 {
+    int memindex = GetMemIndex(n);
+    int mask = GetMemMask(n);
+    TELEM[memindex] = TELEM[memindex] | mask;
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
+    int memindex = GetMemIndex(n);
+    int mask = GetMemMask(n);
+    mask = ~mask;
+    TELEM[memindex] = TELEM[memindex] & mask;
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-  return 0;
+    int memindex = GetMemIndex(n);
+    int mask = GetMemMask(n);
+    return TELEM[memindex] & mask;
+  //return 0;
 }
 
 // битовые операции
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+    TBitField(bf);
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
 {
-  return 0;
+    // последний элемент массива нужно сравнивать побитово
 }
 
 int TBitField::operator!=(const TBitField &bf) const // сравнение

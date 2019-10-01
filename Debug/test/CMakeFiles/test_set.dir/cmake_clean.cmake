bba9d2b0d5f116e6a097464eeae2bd5d40ed1a2c
VizgalovAI <vizgalov.anton.2012@yandex.ru>
2019-10-01 16:25:23 +0300

    Added CMake files

---
+++ Debug/test/CMakeFiles/test_set.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_set.dir/test_main.cpp.o"
+  "CMakeFiles/test_set.dir/test_tbitfield.cpp.o"
+  "CMakeFiles/test_set.dir/test_tset.cpp.o"
+  "../bin/test_set.pdb"
+  "../bin/test_set"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_set.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
